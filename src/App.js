bb7bed2f3507131bef00faed66f4076c8545bf3c
ricksy7745 <beckricks@gmail.com>
2024-02-18 20:22:59 -0700

    Check edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Checking how we publish
+          Edit 2
         </p>
         <a
           className="App-link"
